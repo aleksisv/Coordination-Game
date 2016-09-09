7da939a4080a2906fe84fdbaefe88ed631f8d2ca
aleksisv <aleksis.vuoksenmaa@helsinki.fi>
2016-09-09 14:48:44 +0300

    Some changes to Game-class. Added Pair-class.

--- CoordinationGame/src/main/java/main/Game.java
+++ CoordinationGame/src/main/java/main/Game.java
@@ -6,20 +6,20 @@ import java.util.ArrayList;
 public class Game {
     private int time;
     private ArrayList<Player> players;
-    private int[][] payoffMatrix;
+    private Pair[][] payoffMatrix;
 
     public Game() {
         this.time = 0;
         this.players = generatePlayers();
-        payoffMatrix = new int[2][2];
-        
+        payoffMatrix = new Pair[2][2];
         setPayoffs();
+        play();
         
     }
     
     public void play(){
         while(this.time < 1000) {
-            
+            playTurn();
         }
     }
     
@@ -34,6 +34,21 @@ public class Game {
     }
     
     public void setPayoffs(){
+        Pair[][] payoffs = new Pair[2][2];
+        payoffs[0][0] = new Pair(10, 10);
+        payoffs[0][1] = new Pair(0, 0);
+        payoffs[1][0] = new Pair(0, 0);
+        payoffs[1][1] = new Pair(5, 5);
+    }
+
+    public void setPayoffMatrix(Pair[][] payoffMatrix) {
+        this.payoffMatrix = payoffMatrix;
+    }
+    
+    
+    
+    public void playTurn(){
+        this.time++;
         
     }
     
---
+++ CoordinationGame/src/main/java/main/Pair.java
@@ -0,0 +1,44 @@
+
+package main;
+
+public class Pair {
+    private int[] values;
+
+    public Pair() {
+        this.values = new int[2];
+    }
+    
+    public Pair (int i1, int i2){
+        this.values = new int[2];
+        this.values[0] = i1;
+        this.values[1] = i2;
+    }
+    
+    public void setValues(int first, int second) {
+        this.values[0] = first;
+        this.values[1] = second;
+    }
+    
+    public void setFirst(int first) {
+        this.values[0] = first;
+    }
+    
+    public void setSecond(int second) {
+        this.values[1] = second;
+    }
+    
+    public int getFirst(){
+        return this.values[0];
+    }
+    
+    public int getSecond(){
+        return this.values[1];
+    }
+
+    public int[] getValues() {
+        return values;
+    }
+    
+    
+    
+}
