96caec8b561ebad6a195f4ff5102ad0c254e89d4
aleksisv <aleksis.vuoksenmaa@helsinki.fi>
2016-10-30 19:42:53 +0200

    Päivitetty.

---
+++ CoordinationGame/src/main/java/main/InputFrame.java
@@ -0,0 +1,14 @@
+
+package main;
+
+import javax.swing.*;
+
+public class InputFrame extends JFrame {
+    public InputFrame() {
+        setTitle("Coordination Game");
+        setSize(300, 200);
+        setLocation(10, 200);
+        
+    }
+    
+}
--- CoordinationGame/src/main/java/main/main.java
+++ CoordinationGame/src/main/java/main/main.java
@@ -1,9 +1,13 @@
 
 package main;
 
+import javax.swing.JFrame;
+
 public class main {
     
     public static void main(String[] args) {
+        JFrame f = new InputFrame();
+        f.show();
         Game game = new Game();
         game.play();
     }
